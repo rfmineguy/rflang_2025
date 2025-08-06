dee587f472e5cfa12834c9246935b59773c84584
Riley Fischer <rfmineguy@gmail.com>
2025-08-06 12:07:07 -0700

    token: Refactor the token out of tokenizer into its own unit

---
+++ src/token.c
@@ -0,0 +1,44 @@
+#include "token.h"
+#include <assert.h>
+#include <stdio.h>
+
+void token_print(token t) {
+  printf("Token { '%s', '%.*s' }\n", token_type_str(t.type), (int)t.len, t.start);
+}
+
+const char* token_type_str(token_type tt) {
+  switch (tt) {
+    case ID:      return "ID";
+    case EQ:      return "EQ";
+    // Operators
+    case PLUS:    return "PLUS";
+    case MINUS:   return "MINUS";
+    case MUL:     return "MUL";
+    case DIV:     return "DIV";
+    case MOD:     return "MOD";
+    // Separators
+    case COLON:   return "COLON";
+    case COMMA:   return "COMMA";
+    case SQUOT:   return "SQUOT";
+    case DQUOT:   return "DQUOT";
+    case ARROW:   return "ARROW";
+    case KEYWORD: return "KEYWORD";
+
+    case LCBRK:   return "LCBRK";
+    case RCBRK:   return "RCBRK";
+    case LSBRK:   return "LSBRK";
+    case RSBRK:   return "RSBRK";
+    case LPAR:    return "LPAR";
+    case RPAR:    return "RPAR";
+
+    case LT:      return "LT";
+    case LTEQ:    return "LTEQ";
+    case GT:      return "GT";
+    case GTEQ:    return "GTEQ";
+
+    case INTLIT:  return "INTLIT";
+    case STRLIT:  return "STRLIT";
+    default:      printf("id: %d\n", tt); assert(0 && "This type doesn't have a string yet...");
+  }
+  assert(0 && "Unreachable");
+}
---
+++ src/token.h
@@ -0,0 +1,29 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+#include <stddef.h>
+
+typedef enum {
+  KEYWORD,
+  ID, EQ, COLON,
+  COMMA, SQUOT, DQUOT,
+  PLUS, MINUS, MUL, DIV, MOD,
+  ARROW,
+  LCBRK, RCBRK,
+  LSBRK, RSBRK,
+  LPAR, RPAR,
+  LT, LTEQ,
+  GT, GTEQ,
+  INTLIT, STRLIT,
+} token_type;
+
+typedef struct {
+  token_type type;
+  const char* start;
+  size_t len;
+} token;
+
+void        token_print(token t); 
+const char* token_type_str(token_type tt);
+
+
+#endif
