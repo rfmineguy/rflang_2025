1b8482ddb3f06323dd71c233c02130637d0d445d
Riley Fischer <rfmineguy@gmail.com>
2025-08-05 22:33:36 -0700

    tokenizer (ReadFile): read file content - if error, report it - if no error, return the content

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -16,3 +16,26 @@ static char msgbuf[500];
     printf("[Regex OK] %s\n", regstr);\
   }\
 };
+
+static result_read_file ReadFile(const char* filepath) {
+  FILE* f = fopen(filepath, "r");
+  if (!f) {
+    fprintf(stderr, "Failed to open '%s'\n", filepath);
+    return result_err(read_file, "Failed to open file");
+  }
+
+  fseek(f, 0, SEEK_END);
+  size_t len = ftell(f);
+  fseek(f, 0, SEEK_SET);
+
+  char* buf = calloc(len, sizeof(char));
+  size_t read;
+  if ((read = fread(buf, sizeof(char), len, f)) != len) {
+    fprintf(stderr, "Failed to read entire file '%s'. Read %zu/%zu bytes\n", filepath, read, len);
+    fclose(f);
+    free(buf);
+    return result_err(read_file, "Failed to read entire file");
+  }
+
+  return result_ok(read_file, buf);
+}
