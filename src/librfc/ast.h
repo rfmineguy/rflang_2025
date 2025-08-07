40efae69368a74951221ad451b73c0c84fcd1b71
Riley Fischer <rfmineguy@gmail.com>
2025-08-07 09:45:46 -0700

    [ast] add some basic astnode types (ident, intlit)

--- src/librfc/ast.h
+++ src/librfc/ast.h
@@ -1,8 +1,17 @@
 #ifndef AST_H
 #define AST_H
 
+typedef enum {
+  AST_IDENT, AST_INTLIT,
+} ast_node_type;
+
 typedef struct {
-  int something;
+  ast_node_type type;
+
+  union {
+    struct { const char* s; int len; } ident;
+    struct { int val; } intlit;
+  };
 } ast_node;
 
 #endif
