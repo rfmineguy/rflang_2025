426a3d12daa5dbf5fc0db93139eb123201a123a1
Riley Fischer <rfmineguy@gmail.com>
2025-08-07 10:36:50 -0700

    [test][dynarray] write test testing normal pushback. making sure the elements go in the right place

--- tests/src/dynarray_token.c
+++ tests/src/dynarray_token.c
@@ -10,3 +10,24 @@ MunitResult dynarray_token_create_test(const MunitParameter * params, void *cont
   dynarray_token_free(&da);
   return MUNIT_OK;
 }
+
+MunitResult dynarray_token_pushback_test(const MunitParameter * params, void *context) {
+  const char* id_content = "hello";
+  const char* strlit_content = "\"this is a string literal\"";
+  dynarray_token da = dynarray_token_create();
+
+  dynarray_token_pushback(&da, (token){.type = ID, .start = id_content, .len = strlen(id_content) });
+  munit_assert_int(da.size, ==, 1);
+  munit_assert_int(da.buffer[0].type, ==, ID);
+  munit_assert_int(da.buffer[0].len, ==, strlen(id_content));
+  munit_assert_ptr_equal(da.buffer[0].start, id_content);
+
+  dynarray_token_pushback(&da, (token){.type = STRLIT, .start = strlit_content, .len = strlen(strlit_content) });
+  munit_assert_int(da.size, ==, 2);
+  munit_assert_int(da.buffer[1].type, ==, STRLIT);
+  munit_assert_int(da.buffer[1].len, ==, strlen(strlit_content));
+  munit_assert_ptr_equal(da.buffer[1].start, strlit_content);
+
+  dynarray_token_free(&da);
+  return MUNIT_OK;
+}
