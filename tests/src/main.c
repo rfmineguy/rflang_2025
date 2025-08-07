dbe1134c7311932a16f4fb9138eda1ad190ebe2f
Riley Fischer <rfmineguy@gmail.com>
2025-08-06 17:10:08 -0700

    test: add some tokenizer_create tests to the runner

--- tests/src/main.c
+++ tests/src/main.c
@@ -8,7 +8,8 @@ void* setup(const MunitParameter params[], void* userdata) {
 void teardown(void* fixture) {}
 
 MunitTest tokenizer_tests[] = {
-  { "/create", tokenizer_create_test, setup, teardown, MUNIT_TEST_OPTION_NONE, NULL},
+  { "/create_invld_path", tokenizer_create_invldpath_test, setup, teardown, MUNIT_TEST_OPTION_NONE, NULL},
+  { "/create_valid_path", tokenizer_create_validpath_test, setup, teardown, MUNIT_TEST_OPTION_NONE, NULL},
   { "/free", tokenizer_free_test, setup, teardown, MUNIT_TEST_OPTION_NONE, NULL},
   { "/run", tokenizer_run_test, setup, teardown, MUNIT_TEST_OPTION_NONE, NULL},
   { NULL, NULL,     NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
