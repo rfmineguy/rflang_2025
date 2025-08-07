0362a36a58ea22d6b33b98c29fcbf73b29b70ece
Riley Fischer <rfmineguy@gmail.com>
2025-08-06 22:11:33 -0700

    [test] start on the stack test suite

---
+++ tests/src/stack_ast_node.c
@@ -0,0 +1,53 @@
+#include "tests.h"
+#include "../../src/librfc/stack_ast_node.h"
+
+MunitResult stack_ast_node_create_test(const MunitParameter *param, void *context) {
+  stack_ast_node stack = stack_ast_node_create();
+  munit_assert_int(stack.size, ==, 0);
+  munit_assert_int(stack.capacity, ==, 10);
+  munit_assert_not_null(stack.buffer);
+  stack_ast_node_free(&stack);
+  return MUNIT_OK;
+}
+
+MunitResult stack_ast_node_push_test1(const MunitParameter *param, void *context) {
+  stack_ast_node stack = stack_ast_node_create();
+  stack_ast_node_push(&stack, (ast_node){.something = 2});
+  result_stack_ast_node_top r = stack_ast_node_top(&stack);
+  munit_assert_true(r.isok);
+  munit_assert_int(r.ok.something, ==, 2);
+
+  stack_ast_node_free(&stack);
+  return MUNIT_OK;
+}
+
+MunitResult stack_ast_node_push_pop_test(const MunitParameter *param, void *context) {
+  stack_ast_node stack = stack_ast_node_create();
+  // []
+  stack_ast_node_push(&stack, (ast_node){.something = 2});
+
+  // [2]
+  result_stack_ast_node_top r = stack_ast_node_top(&stack);
+  munit_assert_true(r.isok);
+  munit_assert_int(r.ok.something, ==, 2);
+
+  // [2, 3]
+  stack_ast_node_push(&stack, (ast_node){.something = 3});
+  r = stack_ast_node_top(&stack);
+  munit_assert_true(r.isok);
+  munit_assert_int(r.ok.something, ==, 3);
+
+  // [2]
+  stack_ast_node_pop(&stack);
+  r = stack_ast_node_top(&stack);
+  munit_assert_true(r.isok);
+  munit_assert_int(r.ok.something, ==, 2);
+
+  // [] after pop stack is empty here, so looking at top should fail
+  stack_ast_node_pop(&stack);
+  r = stack_ast_node_top(&stack);
+  munit_assert_true(r.isfail);
+
+  stack_ast_node_free(&stack);
+  return MUNIT_OK;
+}
