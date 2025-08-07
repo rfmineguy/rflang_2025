9c6ab5b74ecfe8c43573ebd25a77cbd5a6ba868d
Riley Fischer <rfmineguy@gmail.com>
2025-08-06 17:08:27 -0700

    test[tokenizer]: create some basic and incomplete tests for the tokenizer

--- tests/src/tests.h
+++ tests/src/tests.h
@@ -2,7 +2,8 @@
 #define TESTS_H
 #include "../lib/munit.h"
 
-MunitResult tokenizer_create_test(const MunitParameter*, void*);
+MunitResult tokenizer_create_invldpath_test(const MunitParameter*, void*);
+MunitResult tokenizer_create_validpath_test(const MunitParameter*, void*);
 MunitResult tokenizer_free_test(const MunitParameter*, void*);
 MunitResult tokenizer_run_test(const MunitParameter*, void*);
 
--- tests/src/tokenizer.c
+++ tests/src/tokenizer.c
@@ -1,7 +1,18 @@
 #include "tests.h"
+#include "../../src/librfc/tokenizer.h"
 
-MunitResult tokenizer_create_test(const MunitParameter *param, void *context){
-  return MUNIT_ERROR;
+MunitResult tokenizer_create_invldpath_test(const MunitParameter *param, void *context){
+  result_tokenizer_create t = tokenizer_create("");
+  munit_assert_true(t.isfail);
+  munit_assert_false(t.isok);
+  return MUNIT_OK;
+}
+
+MunitResult tokenizer_create_validpath_test(const MunitParameter *param, void *context){
+  result_tokenizer_create t = tokenizer_create("samples/main.rf");
+  munit_assert_true(t.isok);
+  munit_assert_false(t.isfail);
+  return MUNIT_OK;
 }
 
 MunitResult tokenizer_free_test(const MunitParameter *param, void *context){
