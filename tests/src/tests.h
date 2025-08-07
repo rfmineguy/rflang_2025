ffd4a53be69eb20f31b819f899c6f1e4baf1abb5
Riley Fischer <rfmineguy@gmail.com>
2025-08-06 22:15:19 -0700

    [test] add stack test functions to the tests header

--- tests/src/tests.h
+++ tests/src/tests.h
@@ -6,4 +6,8 @@ MunitResult tokenizer_create_invldpath_test(const MunitParameter*, void*);
 MunitResult tokenizer_create_validpath_test(const MunitParameter*, void*);
 MunitResult tokenizer_run_test(const MunitParameter*, void*);
 
+MunitResult stack_ast_node_create_test(const MunitParameter *, void *);
+MunitResult stack_ast_node_push_test1(const MunitParameter *, void *);
+MunitResult stack_ast_node_push_pop_test(const MunitParameter *, void *);
+
 #endif
