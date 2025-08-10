2ea504ebde77fae6317da9e707d8d051af709dcd
Riley Fischer <rfmineguy@gmail.com>
2025-08-10 09:06:42 -0700

    AST: Replace classic union pattern with a new variant based pattern

--- src/librfc/ast.h
+++ src/librfc/ast.h
@@ -1,18 +1,17 @@
 #ifndef AST_H
 #define AST_H
 #include "token.h"
+#include "variant.h"
 
-typedef enum {
-  AST_TOKEN, AST_VAR
-} ast_node_type;
+typedef struct { token t; } ast_token;
+typedef struct { token id; token type; } ast_vardecl;
+typedef struct { const char* placeholder; } ast_expr;
 
-typedef struct {
-  ast_node_type type;
+#define ast_node_variant(X)\
+  X(Token, ast_node, ast_token)\
+  X(Var,   ast_node, ast_vardecl)\
+  X(Expr,  ast_node, ast_expr)
 
-  union {
-    struct { token t; } token;
-    struct { int placeholder; } var;
-  };
-} ast_node;
+define_variant(ast_node, ast_node_variant);
 
 #endif
