8fc08a57a46522a7faff258e932b9ebd15500dab
Riley Fischer <rfmineguy@gmail.com>
2025-08-13 21:11:51 -0700

    [ast] implement ast_rel_print, math_expr_print, term_print, factor_print

--- src/librfc/ast.c
+++ src/librfc/ast.c
@@ -61,13 +61,51 @@ void ast_log_conj_print(const variant_ast_log_conj* v, int depth) {
   printf(INDENT_FMT "}\n", INDENT_ARGS);
 }
 void ast_rel_print(const variant_ast_rel* v, int depth) {
+  printf(INDENT_FMT "MathExpr {\n", INDENT_ARGS);
   const variant_ast_rel v2 = *v;
   match_variant(v2, ast_rel, {
-    variant_case(ast_relation, RelME, {})
-    variant_case(ast_relation, ME, {})
+    variant_case(ast_relation, RelME, {
+      printf(INDENT_FMT "Operator: %.*s\n", INDENT_ARGSN(1), TOKEN_ARGS(v2.RelME.operator));
+      ast_rel_print(v2.RelME.rel, depth + 1);
+      ast_math_expr_print(v2.RelME.math_expr, depth + 1);
+    });
+    variant_case(ast_relation, ME, {
+      ast_math_expr_print(v2.ME.conj, depth + 1);
+    })
   })
+  printf(INDENT_FMT "}\n", INDENT_ARGS);
 }
-void ast_log_factor_print(const variant_ast_factor* v, int depth) {
+void ast_math_expr_print(const variant_ast_math_expr* v, int depth) {
+  printf(INDENT_FMT "MathExpr {\n", INDENT_ARGS);
+  const variant_ast_math_expr v2 = *v;
+  match_variant(v2, ast_math_expr, {
+    variant_case(ast_math_expr, METerm, {
+      printf(INDENT_FMT "Operator: %.*s\n", INDENT_ARGSN(1), TOKEN_ARGS(v2.METerm.operator));
+      ast_math_expr_print(v2.METerm.math_expr, depth + 1);
+      ast_term_print(v2.METerm.term, depth + 1);
+    })
+    variant_case(ast_math_expr, Term, {
+      ast_term_print(v2.Term.term, depth + 1);
+    })
+  })
+  printf(INDENT_FMT "}\n", INDENT_ARGS);
+}
+void ast_term_print(const variant_ast_term* v, int depth) {
+  printf(INDENT_FMT "Term {\n", INDENT_ARGS);
+  const variant_ast_term v2 = *v;
+  match_variant(v2, ast_term, {
+    variant_case(ast_term, TermFactor, {
+      ast_factor_print(v2.TermFactor.factor, depth + 1);
+    })
+    variant_case(ast_term, TermOpFactor, {
+      printf(INDENT_FMT "Operator: %.*s\n", INDENT_ARGSN(1), TOKEN_ARGS(v2.TermOpFactor.operator));
+      ast_term_print(v2.TermOpFactor.term, depth + 1);
+      ast_factor_print(v2.TermOpFactor.factor, depth + 1);
+    })
+  })
+  printf(INDENT_FMT "}\n", INDENT_ARGS);
+}
+void ast_factor_print(const variant_ast_factor* v, int depth) {
   printf(INDENT_FMT "Factor {\n", INDENT_ARGS);
   const variant_ast_factor v2 = (*v);
   match_variant(v2, ast_factor, {
@@ -79,9 +117,14 @@ void ast_log_factor_print(const variant_ast_factor* v, int depth) {
 
 void ast_node_print(variant_ast_node n, int depth) {
   match_variant(n, ast_node, {
-    variant_case(ast_node, Token,       { ast_token_print(n.Token, depth + 1); })
-    variant_case(ast_node, Var,         { ast_var_print(n.Var, depth + 1); })
-    variant_case(ast_node, VariantLit,  { ast_lit_print(n.VariantLit, depth + 1); })
-    variant_case(ast_node, VariantExpr, { ast_expr_print(n.VariantExpr, depth + 1); })
+    variant_case(ast_node, Token,           { ast_token_print(n.Token, depth + 1); })
+    variant_case(ast_node, Var,             { ast_var_print(n.Var, depth + 1); })
+    variant_case(ast_node, VariantLit,      { ast_lit_print(n.VariantLit, depth + 1); })
+    variant_case(ast_node, VariantLogDisj,  { ast_log_disj_print(n.VariantLogDisj, depth + 1); })
+    variant_case(ast_node, VariantLogConj,  { ast_log_conj_print(n.VariantLogConj, depth + 1); })
+    variant_case(ast_node, VariantRelation, { ast_rel_print(n.VariantRelation, depth + 1); })
+    variant_case(ast_node, VariantMathExpr, { ast_math_expr_print(n.VariantMathExpr, depth + 1); })
+    variant_case(ast_node, VariantTerm,     { ast_term_print(n.VariantTerm, depth + 1); })
+    variant_case(ast_node, VariantFactor,   { ast_factor_print(n.VariantFactor, depth + 1); })
   })
 }
