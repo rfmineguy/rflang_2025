fd021558f3e31a8d916a6bd97186ce7da8784aa3
Riley Fischer <rfmineguy@gmail.com>
2025-08-19 09:20:47 -0700

    [test,redirect] implement test for redirecting empty text

---
+++ tests/src/redirect.c
@@ -0,0 +1,11 @@
+#include "tests.h"
+#include "../lib/test_util.h"
+#define TEMP_FILE "file.tmp"
+
+MunitResult redirect_empty(const MunitParameter *param, void *context) {
+  redirect_begin(STDOUT_FILENO, TEMP_FILE, handle);
+  redirect_end(STDOUT_FILENO, handle);
+
+  munit_assert_file_contents_equal(TEMP_FILE, "");
+  return MUNIT_OK;
+}
