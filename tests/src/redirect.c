0c3333d415ad2c525dd70bf16aaacd232a769eb1
Riley Fischer <rfmineguy@gmail.com>
2025-08-19 10:49:22 -0700

    [test,redirect] implement test for redirecting text twice in a row (second redirect should overwrite the first)

--- tests/src/redirect.c
+++ tests/src/redirect.c
@@ -27,3 +27,17 @@ MunitResult redirect_oneline_no_newline(const MunitParameter *param, void *conte
   munit_assert_file_contents_equal(TEMP_FILE, "Hello world");
   return MUNIT_OK;
 }
+
+MunitResult redirect_twice(const MunitParameter *param, void *context) {
+  redirect_begin(STDOUT_FILENO, TEMP_FILE, handle);
+  printf("Hello world");
+  redirect_end(STDOUT_FILENO, handle);
+  munit_assert_file_contents_equal(TEMP_FILE, "Hello world");
+
+  redirect_begin(STDOUT_FILENO, TEMP_FILE, handle2);
+  printf("Here is another redirect");
+  redirect_end(STDOUT_FILENO, handle2);
+
+  munit_assert_file_contents_equal(TEMP_FILE, "Here is another redirect");
+  return MUNIT_OK;
+}
