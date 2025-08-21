d13e9e5d8006ca72f71b89f11715116a9ff725a6
Riley Fischer <rfmineguy@gmail.com>
2025-08-21 13:57:33 -0700

    [ast_reconstruct] create function templates for the reconstruction functions

---
+++ src/librfc/ast_reconstruct.h
@@ -0,0 +1,16 @@
+#ifndef AST_RECONSTRUCT_H
+#define AST_RECONSTRUCT_H
+#include "ast.h"
+
+void ast_var_reconstruct(const variant_ast_vardecl*);
+void ast_lit_reconstruct(const variant_ast_lit*);
+void ast_expr_reconstruct(const variant_ast_expr*);
+void ast_log_disj_reconstruct(const variant_ast_log_disj*);
+void ast_log_conj_reconstruct(const variant_ast_log_conj*);
+void ast_rel_reconstruct(const variant_ast_rel*);
+void ast_math_expr_reconstruct(const variant_ast_math_expr*);
+void ast_term_reconstruct(const variant_ast_term*);
+void ast_factor_reconstruct(const variant_ast_factor*);
+void ast_reconstruct(variant_ast_node);
+
+#endif
