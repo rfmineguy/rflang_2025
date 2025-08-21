b6d11624e628e64cfdf3b67c9e15567346a8b40f
Riley Fischer <rfmineguy@gmail.com>
2025-08-21 14:49:35 -0700

    [test, ast_reconstruct] test vardecl reconstruction

---
+++ tests/src/ast_reconstruct.c
@@ -0,0 +1,33 @@
+#include "tests.h"
+#include "test_util.h"
+#include "tokenizer.h"
+#include "parser.h"
+#include "ast_reconstruct.h"
+#define TEMP_FILE "file.tmp"
+
+MunitResult ast_reconstruct_vardecl(const MunitParameter* params, void *context) {
+  result_tokenizer_create r = tokenizer_create_cstr("a: int");
+  munit_assert_true(r.isok);
+  result_tokenizer_run r2 = tokenizer_run(&r.ok);
+  munit_assert_true(r2.isok);
+  munit_assert_int(r.ok.tokens.size, ==, 4);
+
+  result_parser_run r3 = parser_run(&r.ok);
+  munit_assert_true(r3.isok);
+  munit_assert_int(r3.ok.ast_stack.size, ==, 2);
+
+  match(stack_ast_node_top_offset(&r3.ok.ast_stack, 1), stack_ast_node_top, {
+    munit_assert_int(result_.ok.type, ==, variant_ast_node_type_VariantVar);
+
+    redirect_begin(STDOUT_FILENO, TEMP_FILE, x);
+    ast_var_reconstruct(result_.ok.VariantVar);
+    redirect_end(STDOUT_FILENO, x);
+
+    munit_assert_file_contents_equal(TEMP_FILE, "a: int");
+  }, {
+    munit_assert(false);
+  });
+
+  tokenizer_free(&r.ok);
+  return MUNIT_OK;
+}
