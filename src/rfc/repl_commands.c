9120201f86762e921d923b1223f6067724145e32
Riley Fischer <rfmineguy@gmail.com>
2025-09-01 15:52:37 -0700

    [repl] implement tokenize and parse commands

--- src/rfc/repl_commands.c
+++ src/rfc/repl_commands.c
@@ -1,4 +1,7 @@
 #include "repl_commands.h"
+#include "tokenizer.h"
+#include "parser.h"
+#include "result.h"
 #include <stdio.h>
 
 static void repl_help_tokenize() {
@@ -16,7 +19,17 @@ repl_state repl_command_tokenize(repl_ctx* ctx) {
   if (cmd.type == CMD_EXIT) return STATE_DEFAULT;
   if (cmd.type == CMD_HELP) repl_help_tokenize();
   else {
-
+    tokenizer t;
+    match(tokenizer_create_cstr(cmd.cmdbuf), tokenizer_create, { t = result_.ok; }, { return STATE_DEFAULT; });
+    match(tokenizer_run(&t), tokenizer_run, {
+      for (int i = 0; i < t.tokens.size; i++) {
+        token_print(t.tokens.buffer[i]);
+      }
+    }, {
+      printf("Error: %s\n", result_.err);
+      tokenizer_free(&t);
+      return STATE_DEFAULT;
+    });
   }
   return STATE_TOKENIZE;
 }
@@ -26,7 +39,20 @@ repl_state repl_command_parse(repl_ctx* ctx) {
   if (cmd.type == CMD_EXIT) return STATE_DEFAULT;
   if (cmd.type == CMD_HELP) repl_help_parse();
   else {
-
+    tokenizer t;
+    match(tokenizer_create_cstr(cmd.cmdbuf), tokenizer_create, { t = result_.ok; }, { return STATE_DEFAULT; });
+    match(tokenizer_run(&t), tokenizer_run, {
+      match(parser_run(&t), parser_run, {
+        stack_ast_node_print(&result_.ok.ast_stack);
+        parser_free(&result_.ok);
+        tokenizer_free(&t);
+      }, {
+        printf("Error: %s\n", result_.err);
+      })
+    }, {
+      printf("Error: %s\n", result_.err);
+      tokenizer_free(&t);
+    });
   }
   return STATE_PARSE;
 }
