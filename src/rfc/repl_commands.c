39028692b01c1d3844996942b1e53af228945169
Riley Fischer <rfmineguy@gmail.com>
2025-09-01 13:42:40 -0700

    [repl] finish boilerplate implementation for repl_commands

--- src/rfc/repl_commands.c
+++ src/rfc/repl_commands.c
@@ -9,3 +9,24 @@ static void repl_help_parse() {
   printf("Help: parse\n");
   printf("   Type text into the prompt and it will be output as an ast\n");
 }
+
+repl_state repl_command_tokenize(repl_ctx* ctx) {
+  if (!repl_prompt(ctx)) return STATE_TOKENIZE;
+  repl_command cmd = repl_parse_command(ctx);
+  if (cmd.type == CMD_EXIT) return STATE_DEFAULT;
+  if (cmd.type == CMD_HELP) repl_help_tokenize();
+  else {
+
+  }
+  return STATE_TOKENIZE;
+}
+repl_state repl_command_parse(repl_ctx* ctx) {
+  if (!repl_prompt(ctx)) return STATE_PARSE;
+  repl_command cmd = repl_parse_command(ctx);
+  if (cmd.type == CMD_EXIT) return STATE_DEFAULT;
+  if (cmd.type == CMD_HELP) repl_help_parse();
+  else {
+
+  }
+  return STATE_PARSE;
+}
