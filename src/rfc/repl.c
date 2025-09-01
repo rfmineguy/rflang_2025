1efac6c716823994e8c0c382cc1d3645c49ee36a
Riley Fischer <rfmineguy@gmail.com>
2025-09-01 13:45:25 -0700

    [repl] implement function for parsing repl commands

--- src/rfc/repl.c
+++ src/rfc/repl.c
@@ -10,3 +10,23 @@ const char* repl_state_str(repl_state state) {
     case STATE_TOKENIZE: return "Tokenize";
   }
 }
+
+repl_command repl_parse_command(repl_ctx* ctx) {
+  repl_command cmd = {};
+  char* token = ctx->repl_buffer;
+  char* end = ctx->repl_buffer;
+
+  while (token) {
+    strsep(&end, " \n");
+    cmd.args[cmd.argcount++] = token;
+    token = end;
+  }
+
+  cmd.type = CMD_UNKNOWN;
+  if (strcmp(cmd.args[0], "tokenize") == 0) cmd.type = CMD_TOKENIZE;
+  if (strcmp(cmd.args[0], "parse") == 0)    cmd.type = CMD_PARSE;
+  if (strcmp(cmd.args[0], "help") == 0)     cmd.type = CMD_HELP;
+  if (strcmp(cmd.args[0], "exit") == 0)     cmd.type = CMD_EXIT;
+
+  return cmd;
+}
