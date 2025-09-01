049567eac404b1a3f6595c3f2af7d82c92fd2875
Riley Fischer <rfmineguy@gmail.com>
2025-09-01 13:48:48 -0700

    [repl] implement the repl into the main executable; use gengetopt as well

--- src/rfc/main.c
+++ src/rfc/main.c
@@ -1,11 +1,21 @@
 #include "librfc/tokenizer.h"
 #include "librfc/parser.h"
 #include "spall_util.h"
+#include "cmdline.h"
+#include "repl.h"
 #include <stdio.h>
 
-int main() {
+int main(int argc, char** argv) {
   SPALL_INIT
   tokenizer t;
+
+  struct gengetopt_args_info args_info;
+  if (cmdline_parser(argc, argv, &args_info) != 0) return 1;
+
+  if (args_info.repl_given) {
+    return repl_run();
+  }
+
 
   // Create the tokenizer
   match(tokenizer_create_file("samples/main.rf"), tokenizer_create, t = result_.ok;,{
