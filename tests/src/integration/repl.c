67707550af6e54106813fded05e09abf4cbcb936
Riley Fischer <rfmineguy@gmail.com>
2025-09-09 15:20:09 -0700

    [test; repl] - experiment with a way to test the repl (method may not work on   windows, untested)

---
+++ tests/src/integration/repl.c
@@ -0,0 +1,43 @@
+#include "tests.h"
+#include <unistd.h>
+#include <stdio.h>
+#ifndef RFC_PATH
+#define RFC_PATH ""
+#endif
+
+MunitResult rfc_repl_test_blank_input(const MunitParameter* params, void* fixture) {
+  munit_assert_string_not_equal(RFC_PATH, ""); //TODO: Replace with an assertion that the path exists
+
+  // setup mock input
+#define input\
+  "parse\n"\
+  "a + 3\n"\
+
+#define expected_output\
+  "Expr.Disj.Conj.Rel.MathExpr {\n"\
+  "  Operator: +\n"\
+  "  MathExpr.Term.Factor.Lit{id: a}\n"\
+  "  Term.Factor.Lit{int: 3}\n"\
+  "}\n"\
+  "Token(EOF_), ''\n"\
+
+  // setup input file
+  FILE* f = fopen("input.txt", "w");
+  fputs(input, f);
+  fclose(f);
+
+  // run executable with redirected IO
+  int ret = system(RFC_PATH " --repl" " < input.txt > output.txt 2> prompts.txt");
+  if (WIFEXITED(ret)) {
+    // int status = WEXITSTATUS(ret);
+  }
+  else {
+    printf("Child did not exit normally\n");
+    return MUNIT_ERROR;
+  }
+  if (ret == -1) return MUNIT_ERROR;
+
+  munit_assert_file_contents_equal("output.txt", expected_output);
+
+  return MUNIT_OK;
+}
