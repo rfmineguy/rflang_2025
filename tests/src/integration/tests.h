29308e1292606363a90aa793a3af2843e3c03c20
Riley Fischer <rfmineguy@gmail.com>
2025-09-09 15:22:08 -0700

    [test] setup tests.h for the integration tests

---
+++ tests/src/integration/tests.h
@@ -0,0 +1,8 @@
+#ifndef INTEGRATION_TESTS_H
+#define INTEGRATION_TESTS_H
+#include "munit.h"
+#include "munit_ext.h"
+
+MunitResult rfc_repl_test_blank_input(const MunitParameter*, void*);
+
+#endif
