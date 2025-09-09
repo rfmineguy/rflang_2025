f7127639c4f8814348241c24b110eaf6e84c1713
Riley Fischer <rfmineguy@gmail.com>
2025-09-09 15:08:31 -0700

    [Tokenizer] delete dead code from the first implementations

--- src/librfc/tokenizer.c
+++ src/librfc/tokenizer.c
@@ -98,26 +98,6 @@ static result_read_file ReadFile(const char* filepath) {
   buffer[size] = '\0'; // Null terminate
   fclose(f);
   return result_ok(read_file, buffer);
-  // FILE* f = fopen(filepath, "r");
-  // if (!f) {
-  //   fprintf(stderr, "Failed to open '%s'\n", filepath);
-  //   return result_err(read_file, "Failed to open file");
-  // }
-
-  // fseek(f, 0, SEEK_END);
-  // size_t len = ftell(f);
-  // fseek(f, 0, SEEK_SET);
-
-  // char* buf = calloc(len, sizeof(char));
-  // size_t read;
-  // if ((read = fread(buf, sizeof(char), len, f)) != len) {
-  //   fprintf(stderr, "Failed to read entire file '%s'. Read %zu/%zu bytes\n", filepath, read, len);
-  //   fclose(f);
-  //   free(buf);
-  //   return result_err(read_file, "Failed to read entire file");
-  // }
-
-  // return result_ok(read_file, buf);
 }
 
 result_tokenizer_create tokenizer_create_file(const char *filepath) {
