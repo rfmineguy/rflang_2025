15109c9092ef5a9017244c55c192739c022dd7c0
Riley Fischer <rfmineguy@gmail.com>
2025-09-10 09:30:43 -0700

    [test] rebind file descriptors for windows crt

--- tests/lib/test_util.c
+++ tests/lib/test_util.c
@@ -11,6 +11,27 @@ void redirect_begin_(FILE* in, FILE* out, FILE* err, stdio_ctx* save) {
   if (in)  dup2(fileno(in), 0);
   if (out) dup2(fileno(out), 1);
   if (err) dup2(fileno(err), 2);
+
+  // for windows we need to rebind stdout, and stderr to the new file descriptors
+  // so the windows CRT knows about them
+#ifdef _WIN32
+  if (out) {
+      FILE *newOut = _fdopen(dup(1), "w");
+      if (newOut) {
+          *stdout = *newOut;           // copy FILE internals
+          setvbuf(stdout, NULL, _IONBF, 0); 
+          fclose(newOut);              // stdout now owns fd 1
+      }
+  }
+  if (err) {
+      FILE *newErr = _fdopen(dup(2), "w");
+      if (newErr) {
+          *stderr = *newErr;
+          setvbuf(stderr, NULL, _IONBF, 0);
+          fclose(newErr);
+      }
+  }
+#endif
 }
 void redirect_end_(stdio_ctx* save) {
   fflush(stdout);
